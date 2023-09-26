c222e5fa1b680d4c17d249169b55fdec5bd36094
Aeromosca <146101013+Aeromosca@users.noreply.github.com>
2023-09-26 14:07:26 +0000

    ex4

---
+++ Avaliação/Ex04.py
@@ -0,0 +1,11 @@
+import numpy as np
+N = 4
+matriz = np.zeros((N, N))
+
+for l in range(len(matriz)):
+    for c in range(len(matriz[l])):
+        matriz[l][c] = int(input(f"Insira o valor {l}, {c} para a matriz: "))
+
+matriz = 
+
+print(matriz)
